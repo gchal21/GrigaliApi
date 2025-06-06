4efb3b6f5df550ffff34a369fff57adc55b3cc86
GiorgiChalagashvili <chalagashvili.giorgi@gmail.com>
2025-06-06 23:18:24 +0400

    delete unnecessary files

--- FirstSparrow.Application/Domain/Entities/Exceptions/ExceptionCode.cs
+++
@@ -1,9 +0,0 @@
-namespace FirstSparrow.Application.Domain.Exceptions;
-
-public enum ExceptionCode
-{
-    None = 0,
-    GENERAL_ERROR = 1,
-    OBJECT_NOT_FOUND = 2,
-    DEPOSIT_ALREADY_EXISTS = 3,
-}
--- FirstSparrow.Application/Domain/Entities/Models/Deposit.cs
+++
@@ -1,5 +0,0 @@
-using FirstSparrow.Application.Domain.Entities;
-
-namespace FirstSparrow.Application.Domain.Models;
-
-public class Deposit() : MerkleNode(0);
